ea8ce4e5a75a0be8d796a712aeda1eb3c7c1eaaa
louresb <bruno.loures@hotmail.com>
2022-12-26 20:10:50 -0300

    Desafio concluído!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,19 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        //*IMPLEMENTADO*
+        private int Memoria;
+        private string IMEI;
+        private string Modelo;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            //*IMPLEMENTADO*
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//*TESTE REALIZADO
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("888359486","Nokia G21","67831569843",128);
+nokia.Ligar();
+nokia.InstalarAplicativo("DIO.NetApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone("777359486","Iphone 11","375498653215",64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DIO.NetApp");
+
+
+
